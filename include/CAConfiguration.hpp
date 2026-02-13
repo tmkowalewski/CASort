dca4bc2f8f99f223892a0261c67b9a41881085bb
T. M. Kowalewski <tylermkowalewski@gmail.com>
2026-02-13 14:21:46 -0500

    move histogram flags out of library

--- include/CAConfiguration.hpp
+++ include/CAConfiguration.hpp
@@ -5,17 +5,15 @@
 #include <algorithm>
 #include <thread>
 
+// Which modules to process
+#define PROCESS_POS_SIG false
+#define PROCESS_CEBR_ALL false
+
 // Number of hardware threads to use in processing
-const unsigned int kMaxThreads = std::min(15U, std::thread::hardware_concurrency()); // Number of threads to use for processing, defaults to system max
+const unsigned int kMaxThreads = std::min(20U, std::thread::hardware_concurrency()); // Number of threads to use for processing, defaults to system max
 
 // Debug Mode
 #define DEBUG 1
-
-// Which modules to process
-#define PROCESS_CLOVER_CROSS true
-#define PROCESS_CLOVER_BACK true
-#define PROCESS_POS_SIG false
-#define PROCESS_CEBR_ALL false
 
 // Calibration File Name Templates
 #define RUN_FILE_NAME_TEMPLATE "root_data_70Ge_run%03d.mvmelst.bin_tree.root"
